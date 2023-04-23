cc5aabef7991f5495061d6074777caccde2afd83
JackyBlackson <Jacky_Blackson@outlook.com>
2023-04-23 19:35:04 +0800

    fix: 修复因为TCP分包导致消息被截断识别
    建立缓冲区来收集byte数据直到遇到了消息尾

--- server/src/main/java/jdbms/server/connection/ClientHandler.java
+++ server/src/main/java/jdbms/server/connection/ClientHandler.java
@@ -4,10 +4,10 @@ import jdbms.server.user.User;
 import jdbms.server.util.Importance;
 import jdbms.server.util.Logger;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Jacky_Blackson
@@ -28,15 +28,42 @@ public class ClientHandler implements Runnable {
         try {
             InputStream inputStream = clientSocket.getInputStream();
             OutputStream outputStream = clientSocket.getOutputStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
+            ByteBuffer buffer = ByteBuffer.allocate(100000000); // 创建大小为140960的ByteBuffer缓冲区
+            int bytesRead = 0;
             Logger.Log(Importance.DEBUG, "Client Thread Prepared");
             do {
-                bytesRead = inputStream.read(buffer);
+                int bytesReadeadThisTime = 0;
+                byte[] re = new byte[4096];
+                bytesReadeadThisTime = inputStream.read(re) + bytesRead;
+                System.out.println(bytesRead);
+                System.out.println(bytesReadeadThisTime);
+                System.out.println(buffer.remaining());
+                buffer.position(bytesRead);
+                buffer.put(re, 0, bytesReadeadThisTime);
+                bytesRead += bytesReadeadThisTime;
                 Logger.Log(Importance.DEBUG, "Client Thread Receiving");
-                String message = new String(buffer, 0, bytesRead);
-                System.out.println("Received message from " + clientSocket.getInetAddress().getHostName() + ": " + message);
-                outputStream.write(("Echo: " + message).getBytes());
+                //成功读取
+                if (bytesRead > 0) {
+                    buffer.position(bytesRead);
+                    buffer.flip();
+                    byte[] data = new byte[bytesRead];
+                    buffer.get(data);
+                    String message = new String(data, 0, bytesRead);
+                    //遇到消息尾 #
+                    if (message.contains("#")) {
+                        String fullMessage = message.trim();
+                        //检测消息是否是消息头开始
+                        if (fullMessage.startsWith("$")) {
+                            //此处消息接收完成，交给处理器处理
+                            System.out.println("Received message from " + clientSocket.getInetAddress().getHostName() + ": " + fullMessage);
+                            outputStream.write(("Echo: " + fullMessage).getBytes());
+                        } else {    //消息出现错误，没有检测到消息头：发出警报并忽略这条消息
+                            Logger.Log(Importance.WARNING, "收到了不完整的消息，是否网络或客户端出现错误: " + fullMessage);
+                        }
+                        bytesRead = 0;
+                        buffer.clear();
+                    }
+                }
             } while (bytesRead != -1);
         } catch (IOException e) {
             e.printStackTrace();
@@ -50,4 +77,5 @@ public class ClientHandler implements Runnable {
             }
         }
     }
+
 }
